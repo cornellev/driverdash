b34400577158515a80b94c428bb46b1c423498f3
Jason Klein <jklein.64@verizon.net>
2023-03-24 16:02:42 -0400

    Initial Commit

---
+++ driver-dash-2023/driver_dash_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  driver_dash_2023App.swift
+//  driver-dash-2023
+//
+//  Created by Jason Klein on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct driver_dash_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
