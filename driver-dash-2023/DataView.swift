2d84997a8a8c715519e7e5ee1aec40f749bf66f4
kalehiwot <ktd27@cornell.edu>
2023-03-25 20:13:22 -0500

    Implemented Map Visual
    - Added Map
    - Formatted Map on the right half
    - Formatted Speed over Power

--- driver-dash-2023/DataView.swift
+++ driver-dash-2023/DataView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import MapKit
 
 // see https://engineering.deptagency.com/state-of-swift-websockets
 
@@ -15,17 +16,29 @@ struct DataView: View {
     var units: String
     
     var body: some View {
-        VStack {
-            // title styling
+        HStack {
             Text(title)
                 .font(.title).bold()
-                .padding([.leading, .trailing], 100)
+                .padding([.leading, .trailing], 40)
             Text(units)
             
             // handle data display
             Text(String(format: "%.2f", value))
-                .font(.system(size: 48, design: .monospaced))
+                .font(.system(size: 25, design: .monospaced))
                 .padding(10)
         }
+        HStack{}
+    }
+}
+
+//create a map visual 
+struct MapView: View {
+    @State var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 39.7954, longitude: -86.2353),
+        span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+    )
+
+    var body: some View {
+        Map(coordinateRegion: $region)
     }
 }
