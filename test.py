2008a203a3abbd8e766026eddb11a9162c153285
jak532 <jak532@cornell.edu>
2023-03-27 19:49:39 -0400

    Make a better test file

--- test.py
+++ test.py
@@ -1,26 +1,51 @@
 import socket
 import time
+import random
 
-# set IP address and port number
-IP_ADDRESS = "10.48.155.202"
-PORT = 8080
+from argparse import ArgumentParser
 
-# create a TCP socket object
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def main():
+    parser = ArgumentParser(
+        description="Tests TCP transmission with iPhone app.")
+    
+    parser.add_argument("ip", help="IP address of the phone")
+    parser.add_argument("--repetitions", "-r", metavar="n", type=int, default=1, required=False,
+        help="Number of times to repeat each request")
 
-# connect to the server
-client_socket.connect((IP_ADDRESS, PORT))
+    args = parser.parse_args()
 
-# send a message to the server
-for _ in range(10):
-    message = r'{"voltage": 24.1, "safety": 1, "rpm": 300}'
-    client_socket.sendall(len(message).to_bytes(4, "little"))
-    client_socket.sendall(message.encode())
-    time.sleep(10 / 1000)
+    print("Sending payload to front server")
+    # create the TCP socket object
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((args.ip, 8081))
+    # send a message to the server
+    for _ in range(args.repetitions):
+        message = '{{"throttle": {throttle}, "angle": {angle}, "tof": {tof}}}'.format(
+            throttle=random.randrange(0, 100),
+            angle=random.randrange(0, 359),
+            tof=random.random())
+        print(message)
+        client_socket.sendall(len(message).to_bytes(4, "little"))
+        client_socket.sendall(message.encode())
+        time.sleep(.25)
+    client_socket.close()
 
-# receive a response from the server
-#response = client_socket.recv(1024)
-#print(f"Received message from server: {response.decode()}")
+    print("Sending payload to back server")
+    # create the TCP socket object
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((args.ip, 8080))
+    # send a message to the server
+    for _ in range(args.repetitions):
+        message = '{{"voltage": {voltage}, "safety": {safety}, "rpm": {rpm}}}'.format(
+            voltage=random.random() * 1000,
+            safety=0 if random.random() < .5 else 1,
+            rpm=random.randrange(0, 50000))
+        print(message)
+        client_socket.sendall(len(message).to_bytes(4, "little"))
+        client_socket.sendall(message.encode())
+        time.sleep(.25)
+    client_socket.close()
 
-# close the socket connection
-client_socket.close()
+
+if __name__ == "__main__":
+    main()
