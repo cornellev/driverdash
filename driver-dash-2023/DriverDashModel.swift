0921dce81b9223354035ef3dbe57e3391118178e
jak532 <jak532@cornell.edu>
2023-03-27 03:45:49 -0400

    Remove old comment

--- driver-dash-2023/DriverDashModel.swift
+++ driver-dash-2023/DriverDashModel.swift
@@ -8,12 +8,6 @@
 import SwiftUI
 import CoreLocation
 
-// have two UserDefaults keys, one for front-daq and one for back-daq
-// each one has the timestamp: { data } format I outlined for Drew
-// note that the location comes with a timestamp!
-
-// see this for clearing it https://developer.apple.com/documentation/foundation/userdefaults/1415919-dictionaryrepresentation
-
 class DriverDashModel: NSObject, ObservableObject {
     @Published var speed = 0.0
     @Published var power = 0.0
