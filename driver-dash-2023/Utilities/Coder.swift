6d557e02ce5df485772b504a3a8b95c32d9dcc76
Jklein64 <jklein.64@verizon.net>
2023-03-27 18:04:00 -0400

    Finish refactoring into Coder

--- driver-dash-2023/Utilities/Coder.swift
+++ driver-dash-2023/Utilities/Coder.swift
@@ -49,60 +49,19 @@ class Coder {
         var rtk: RTK?
     }
     
-    func decode<T: Codable>(from payload: Data, type: T.Type) -> T {
+    // assumes all payloads are valid. Will crash if not!
+    func decode<T: Codable>(from payload: Data, ofType type: T.Type) -> T {
         let decoder = JSONDecoder()
         return try! decoder.decode(T.self, from: payload)
     }
     
-    func decode<T: Codable>(from payload: String, type: T.Type) -> T {
+    func decode<T: Codable>(from payload: String, ofType type: T.Type) -> T {
         let data = payload.data(using: .utf8)!
-        return decode(from: data, type: type)
+        return decode(from: data, ofType: type)
     }
-    
-    // assumes all payloads are valid. Will crash if not!
-    func decodeFrontDAQ(from payload: String) -> FrontPacket {
-        return decodeFrontDAQ(from: payload.data(using: .utf8)!)
-    }
-    
-    func decodeFrontDAQ(from payload: Data) -> FrontPacket {
-        do {
-            return try JSONDecoder().decode(FrontPacket.self, from: payload)
-        } catch let error {
-            print("decodeFrontDAQ error: \(error.localizedDescription)")
-            return FrontPacket()
-        }
-    }
-    
-    func encodeFrontDAQ(from packet: FrontPacket) -> String {
-        do {
-            let encoded = try JSONEncoder().encode(packet)
-            return String(data: encoded, encoding: .utf8)!
-        } catch let error {
-            print("decodeFrontDAQ error: \(error.localizedDescription)")
-            return ""
-        }
-    }
-    
-    func decodeBackDAQ(from payload: String) -> BackPacket {
-        return decodeBackDAQ(from: payload.data(using: .utf8)!)
-    }
-    
-    func decodeBackDAQ(from payload: Data) -> BackPacket {
-        do {
-            return try JSONDecoder().decode(BackPacket.self, from: payload)
-        } catch let error {
-            print("decodeBackDAQ error: \(error.localizedDescription)")
-            return BackPacket()
-        }
-    }
-    
-    func encodeBackDAQ(from packet: BackPacket) -> String {
-        do {
-            let encoded = try JSONEncoder().encode(packet)
-            return String(data: encoded, encoding: .utf8)!
-        } catch let error {
-            print("decodeBackDAQ error: \(error.localizedDescription)")
-            return ""
-        }
+
+    func encode<T: Codable>(from packet: T) -> String {
+        let encoded = try! JSONEncoder().encode(packet)
+        return String(data: encoded, encoding: .utf8)!
     }
 }
