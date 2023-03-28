a10f747158f131b69ed6372b64fdab6f29a5a56b
Jklein64 <jklein.64@verizon.net>
2023-03-28 02:19:04 -0400

    Draw polylines!

--- driver-dash-2023/MapView.swift
+++ driver-dash-2023/MapView.swift
@@ -11,7 +11,7 @@ import MapKit
 // Watch this vid for the bottom structs: https://www.youtube.com/watch?v=hWMkimzIQoU
 // https://codakuma.com/the-line-is-a-dot-to-you/
 final class MapView: NSObject, UIViewRepresentable {
-    var coordinates: [CLLocation]
+    var coordinates: [CLLocationCoordinate2D]
     
     override init() {
         coordinates = []
@@ -22,24 +22,32 @@ final class MapView: NSObject, UIViewRepresentable {
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
         mapView.delegate = context.coordinator
+        // disable interactions
         mapView.interactions = []
+        mapView.isZoomEnabled = false
+        mapView.isScrollEnabled = false
+        mapView.isUserInteractionEnabled = false
+        // center to the map
         mapView.region = MKCoordinateRegion(
             center: CLLocationCoordinate2D(latitude: 39.795, longitude: -86.2353),
             span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.0175))
         
         // see https://stackoverflow.com/a/36827996
-        if let url = Bundle.main.url(forResource: "Indy500", withExtension: ".json") {
-            struct Coordinate: Codable { var lat, long: Double }
-            // [Coordinate] is the type for a Coordinate array
-            let json = try! Coder().decode(from: Data(contentsOf: url), ofType: [Coordinate].self)
-            
-            // fill the coordinates array
-            for coordinate in json {
-                self.coordinates.append(CLLocation(
-                    latitude: coordinate.lat,
-                    longitude: coordinate.long))
-            }
+        let url = Bundle.main.url(forResource: "Indy500", withExtension: ".json")!
+        
+        struct Coordinate: Codable { var lat, long: Double }
+        // [Coordinate] is the type for a Coordinate array
+        let json = try! Coder().decode(from: Data(contentsOf: url), ofType: [Coordinate].self)
+        
+        // fill the coordinates array
+        for coordinate in json {
+            self.coordinates.append(CLLocationCoordinate2D(
+                latitude: coordinate.lat,
+                longitude: coordinate.long))
         }
+        
+        let polyline = MKPolyline(coordinates: coordinates, count: coordinates.count)
+        mapView.addOverlay(polyline)
         
         return mapView
     }
@@ -54,9 +62,22 @@ final class MapView: NSObject, UIViewRepresentable {
 }
 
 class Coordinator: NSObject, MKMapViewDelegate {
-  var parent: MapView
+    var parent: MapView
 
-  init(_ parent: MapView) {
-    self.parent = parent
-  }
+    init(_ parent: MapView) {
+        self.parent = parent
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if let polyline = overlay as? MKPolyline {
+            let renderer = MKPolylineRenderer(polyline: polyline)
+            renderer.strokeColor = UIColor.systemBlue
+            renderer.lineCap = .round
+            renderer.lineJoin = .round
+            renderer.lineWidth = 5
+            return renderer
+        }
+        
+        return MKOverlayRenderer()
+    }
 }
