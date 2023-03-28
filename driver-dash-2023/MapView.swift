d62e97399ab50ffff1192916520a7a67e53e2d36
Jklein64 <jklein.64@verizon.net>
2023-03-28 01:48:28 -0400

    Start following another tutorial

--- driver-dash-2023/MapView.swift
+++ driver-dash-2023/MapView.swift
@@ -9,22 +9,35 @@ import SwiftUI
 import MapKit
 
 // Watch this vid for the bottom structs: https://www.youtube.com/watch?v=hWMkimzIQoU
-struct MapView: View {
-    @StateObject private var model = MapModel()
+// https://codakuma.com/the-line-is-a-dot-to-you/
+struct MapView: UIViewRepresentable {
+    let coordinates: [CLLocation] = []
     
-    @State var region: MKCoordinateRegion = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 39.795, longitude: -86.2353),
-        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.0175)
-    )
+    // Create the MKMapView using UIKit.
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        mapView.interactions = []
+        mapView.region = MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 39.795, longitude: -86.2353),
+            span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.0175))
+        
+        return mapView
+    }
 
-    var body: some View {
-        Map(coordinateRegion: $region, interactionModes: [], showsUserLocation: true)
-            .ignoresSafeArea()
+    // We don't need to worry about this as the view will never be updated.
+    func updateUIView(_ view: MKMapView, context: Context) {}
+
+    // Link it to the coordinator which is defined below.
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
     }
 }
 
-struct MapView_Previews: PreviewProvider {
-    static var previews: some View {
-        MapView()
-    }
+class Coordinator: NSObject, MKMapViewDelegate {
+  var parent: MapView
+
+  init(_ parent: MapView) {
+    self.parent = parent
+  }
 }
