0e91ea17be8b7a05dc93cc1517b9b92c5afbee23
Jklein64 <jklein.64@verizon.net>
2023-03-29 21:44:09 -0400

    Add Phone connection status

--- driver-dash-2023/DDModel.swift
+++ driver-dash-2023/DDModel.swift
@@ -14,6 +14,7 @@ class DDModel: NSObject, ObservableObject {
     
     @Published var daqSocketConnected = false
     @Published var lordSocketConnected = false
+    @Published var phoneConnected = false
     
     var location: CLLocation?
 }
